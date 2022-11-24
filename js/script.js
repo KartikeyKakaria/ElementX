323b2889266530a83e8744c2e3cd5c9effde32d2
KartikeyKakaria <kartikeykakaria2@gmail.com>
2022-11-24 20:54:44 +0530

    symbol css and js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-document.getElementById("submit").addEventListener("click", () => {
+const symbol = document.getElementById("symbol");
+symbol.style.display = "none"
+
+async function fetchdata() {
     const elementName = document.getElementById("elementName").value;
-    fetch(`https://neelpatel05.pythonanywhere.com/element/atomicname?atomicname=${elementName}`)
+    return fetch(`https://neelpatel05.pythonanywhere.com/element/atomicname?atomicname=${elementName}`)
         .then(rep => rep.json())
-        .then(data => console.log(data))
-        .catch(err => console.log(err))
-})
+        .then(data => {
+            return data;
+        })
+        .catch(err => {
+            console.log(err);
+            return err;
+        })
+}
+
+async function displayData() {
+    const element = await fetchdata();
+    if (element.hasOwnProperty("symbol")) {
+        symbol.style.display = "block";
+        symbol.innerHTML = `<h1><span>${element.atomicNumber}</span>${element.symbol}<sub>${Math.floor(Number(element.atomicMass.slice(0,-3)))}</sub></h1>`;
+    }
+    console.log(element)
+}
+document.getElementById("submit").addEventListener("click", displayData)
