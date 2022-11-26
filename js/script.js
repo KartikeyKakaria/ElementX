387acf2397836f895a8c151b1c793eac189b918c
KartikeyKakaria <kartikeykakaria2@gmail.com>
2022-11-26 09:25:48 +0530

    displaying details

--- js/script.js
+++ js/script.js
@@ -16,9 +16,15 @@ async function fetchdata() {
 
 async function displayData() {
     const element = await fetchdata();
+
     if (element.hasOwnProperty("symbol")) {
+        //display symbol
         symbol.style.display = "block";
-        symbol.innerHTML = `<h1><span>${element.atomicNumber}</span>${element.symbol}<sub>${Math.floor(Number(element.atomicMass.slice(0,-3)))}</sub></h1>`;
+        symbol.innerHTML = `<h1><span>${element.atomicNumber}</span>${element.symbol}<sub>${Math.floor(Number(element.atomicMass[0] ||element.atomicMass.slice(0,-3)))}</sub></h1>`;
+
+        //display details
+        const details = document.getElementById("detail")
+        details.innerHTML = `<ul><li>Name:${element.name}</li><li>Atomic Number:${element.atomicNumber}</li><li>Atomic Mass:${Math.floor(Number(element.atomicMass[0] ||element.atomicMass.slice(0,-3)))}</li><li>Year discovered:${element.yearDiscovered}</li><ul>`
     }
     console.log(element)
 }
